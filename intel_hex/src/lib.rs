a26dd6c389c0239f65ed451d263d8d4951e53982
Russ Goetz <russgoetz@gmail.com>
2023-06-26 20:17:41 -0400

    WIP

--- intel_hex/src/lib.rs
+++ intel_hex/src/lib.rs
@@ -1,25 +1,76 @@
 use std::fmt;
+use std::path::Path;
+use std::fs;
+use std::io;
 
-/// let content = std::fs::read("/path/to/intel_hex_file");
-/// let parser = HexFileParser::new(&content)?;
-/// for rec in parser.records() {
-///     // process rec
-/// }
+pub struct HexFileParser<'a> {
+    content: Content<'a>,
+    cursor: &'a [u8],
+}
 
-struct HexFileParser<'a> {
-    content: &'a [u8],
+enum Content<'a> {
+    Borrowed(&'a [u8]),
+    Owned(Vec<u8>),
+}
+
+impl AsRef<[u8]> for Content<'_> {
+    fn as_ref(&self) -> &[u8] {
+        match &self {
+            Content::Borrowed(content) => content,
+            Content::Owned(content) => &content,
+        }
+    }
 }
 
 impl<'a> HexFileParser<'a> {
-    fn new(content: &'a [u8]) -> Result<Self> {
+    /// Creates an Intel Hex file parser from the file's contents.
+    /// 
+    /// # Errors
+    /// 
+    /// This function will return an error if an error is encountered reading the file.
+    /// 
+    /// It will also return an error if the content is not ASCII.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// When exactly don't we have a file????????????????????????????????????????????????
+    /// fn parse_hex_file(content: &[u8]) {
+    ///     let parser = HexFileParser::from_content(content).expect("")
+    /// }
+    /// ```
+    pub fn from_content<T: AsRef<[u8]>>(content: T) -> Result<Self> {
+        let content = content.as_ref();
         if content.is_ascii() {
-            Ok(HexFileParser { content })
+            Ok(HexFileParser { content: Content::Borrowed(content), cursor: content })
         } else {
             Err(Error::NotAscii)
         }
     }
 
-    fn records(&self) -> Records {
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let content = fs::read(path).map_err(Error::ReadFile)?;
+        if content.is_ascii() {
+            Ok(HexFileParser { content: Content::Owned(content), cursor: &content })
+        } else {
+            Err(Error::NotAscii)
+        }
+    }
+
+    /// Returns an iterator over the records in the Intel Hex file.
+    /// 
+    /// # Examples
+    /// 
+    /// Basic usage:
+    /// 
+    /// ```
+    /// let content = std::fs::read("/path/to/intel_hex_file");
+    /// let parser = HexFileParser::new(&content)?;
+    /// for rec in parser.records() {
+    ///     println!("#?", rec);
+    /// }
+    /// ```
+    pub fn records(&self) -> Records {
         Records::new(self.content)
     }
 }
@@ -72,6 +123,7 @@ pub enum RecordKind {
 
 #[derive(Debug)]
 enum Error {
+    ReadFile(io::Error),
     NotAscii,
 }
 
