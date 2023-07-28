dc25cc8b998f3a220bf7741311a5347b19b176a0
Russ Goetz <russgoetz@gmail.com>
2023-07-27 20:24:23 -0400

    WIP

--- decode_intel_hex/src/main.rs
+++ decode_intel_hex/src/main.rs
@@ -1,6 +1,24 @@
-use intel_hex;
+use clap::Parser;
+
+use intel_hex::*;
+
+/*
+Usage:
+  decode_intel_hex -i arduplane.exe -o arduplane.bin
+
+  NOTE: Both arguments are required.
+
+ */
 
 fn main() {
-    let result = intel_hex::add(1, 2);
-    println!("Result = {result}");
+    if let Err(e) = try_main() {
+        eprintln!("ERROR: {}", e);
+    } 
+}
+
+fn try_main() -> anyhow::Result<()> {
+    let records = parse_hex_file("test_files/arduplane.hex")?;
+    let output = process_records(records)?;
+    println!("{} chunks", output.chunks.len());
+    Ok(())
 }
--- intel_hex/src/lib.rs
+++ intel_hex/src/lib.rs
@@ -7,32 +7,3 @@ pub use parse::parse_hex_file;
 pub use process::process_records;
 pub use flash::flash_blocks;
 pub use common::{Record, RecordKind};
-
-/*
-let records = parse_hex_file("/path/to/hexfile")?;  // takes AsRef<Path>, returns Result<Vec<Record>>
-let results = process_records(&records);            // takes &[Record], returns ProcessResults
-let blocks = flash_blocks::<512>(&results.chunks);  // takes &[Chunk], returns Vec<FlashBlock>
-
-struct ProcessResults {
-    chunks: Vec<Chunk>,
-    segment_start: Option<SegmentStart>,
-    linear_start: Option<u32>,
-    ended_with_eof_record: bool,
-}
-
-pub struct SegmentStart {
-    pub cs: u16,
-    pub ip: u16,
-}
-
-pub struct Chunk {
-    pub addr: u32,
-    pub data: Vec<u8>,
-}
-
-impl Chunk {
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
-}
-*/
